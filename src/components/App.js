0873b28a9a5bc589c2883996533b843a174b1b84
Eduardo Marino Riscado <eduardomarino@gmail.com>
2018-07-10 16:08:22 -0300

    Change app title and delete react logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import Planet from './Planet';
 
@@ -62,8 +61,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Star Wars Colonist</h1>
         </header>
         <Planet
           name={ this.state.name }
